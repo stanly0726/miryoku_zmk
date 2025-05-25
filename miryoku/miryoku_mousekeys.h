fa775fa681fe9fe46d677b83435695dae78ca810
stanly@Tower <stanly61315@gmail.com>
2025-05-25 14:06:12 +0800

    feat: zmk mouse emulation support

--- miryoku/miryoku_mousekeys.h
+++ miryoku/miryoku_mousekeys.h
@@ -7,11 +7,11 @@
 #define U_BTN2 &mkp MB2
 #define U_BTN3 &mkp MB3
 
-#define U_MS_D &kp KP_N2
-#define U_MS_L &kp KP_N4
-#define U_MS_R &kp KP_N6
-#define U_MS_U &kp KP_N8
-#define U_WH_D U_NU
-#define U_WH_L U_NU
-#define U_WH_R U_NU
-#define U_WH_U U_NU
+#define U_MS_D &mmv MOVE_DOWN
+#define U_MS_L &mmv MOVE_LEFT
+#define U_MS_R &mmv MOVE_RIGHT
+#define U_MS_U &mmv MOVE_UP
+#define U_WH_D &msc SCRL_DOWN
+#define U_WH_L &msc SCRL_LEFT
+#define U_WH_R &msc SCRL_RIGHT
+#define U_WH_U &msc SCRL_UP
